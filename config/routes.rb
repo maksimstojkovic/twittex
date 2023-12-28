a8071415ed65a8218b489f352b6910273d6f1cb1
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-29 05:41:33 +1100

    Remove unused follow controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
-  resources :follows
-
   #####
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
