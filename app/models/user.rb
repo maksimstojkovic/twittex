99f30a699ea6bcc35f9512c115161fff858a24ec
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-29 04:59:30 +1100

    Fix user follow associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,18 @@ class User < ApplicationRecord
          :confirmable, :lockable, :timeoutable,
          :trackable, :omniauthable
 
-  has_many :follows, foreign_key: :followee
-  has_many :follow_requests, -> { where accepted: false }, through: :follows, source: :follower
-  has_many :followers, -> { where accepted: true }, through: :follows, source: :follower
+  has_many :follows_received, class_name: :Follow, foreign_key: :followee_id
+  has_many :pending_follower_requests, -> { where accepted: false }, class_name: :Follow, foreign_key: :followee_id
+  has_many :accepted_follower_requests, -> { where accepted: true }, class_name: :Follow, foreign_key: :followee_id
+  has_many :pending_followers, through: :pending_follower_requests, source: :follower
+  has_many :followers, through: :accepted_follower_requests, source: :follower
 
+  has_many :follows_sent, class_name: :Follow, foreign_key: :follower_id
+  has_many :pending_following_requests, -> { where accepted: false }, class_name: :Follow, foreign_key: :follower_id
+  has_many :accepted_following_requests, -> { where accepted: true }, class_name: :Follow, foreign_key: :follower_id
+  has_many :pending_followings, through: :pending_following_requests, source: :followee
+  has_many :followings, through: :accepted_following_requests, source: :followee
+  
   # HELPERS
 
   def self.email_find_or_create_by(attributes = {})
