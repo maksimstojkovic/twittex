d76c122715189529eaede9baa70d24c4cab20cb1
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-29 04:25:02 +1100

    Fix mock data variable labels

--- db/seeds/mock.rb
+++ db/seeds/mock.rb
@@ -3,4 +3,5 @@ u2 = User.email_find_or_create_by(name: "user2", username: "handle2", email: "u2
 u3 = User.email_find_or_create_by(name: "user3", username: "handle3", email: "u3@gmail.com", password: "password")
 
 f1 = Follow.find_or_create_by(followee: u1, follower: u2, accepted: false)
-f2 = Follow.find_or_create_by(followee: u1, follower: u3, accepted: true)
+f2 = Follow.find_or_create_by(followee: u2, follower: u1, accepted: false)
+f3 = Follow.find_or_create_by(followee: u1, follower: u3, accepted: true)
