1f8362e5266cca05030b8a9ee20184b64e541206
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-28 22:48:34 +1100

    Generate Follow scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable,
          :trackable, :omniauthable
+
+  has_many :follows, foreign_key: :followee
+  has_many :follow_requests, -> { where accepted: false }, through: :follows, source: :follower
+  has_many :followers, -> { where accepted: true }, through: :follows, source: :follower
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
+  resources :follows
+
   #####
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
