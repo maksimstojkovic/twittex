7ac4db64b6cfd584ad0e41842f6fc6dddaac647c
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-29 04:59:19 +1100

    Add extra follow mock data and bumper messages

--- db/seeds/mock.rb
+++ db/seeds/mock.rb
@@ -1,7 +1,16 @@
+puts "Importing mock data into database"
+
+# Sample users
 u1 = User.email_find_or_create_by(name: "user1", username: "handle1", email: "u1@gmail.com", password: "password")
 u2 = User.email_find_or_create_by(name: "user2", username: "handle2", email: "u2@gmail.com", password: "password")
 u3 = User.email_find_or_create_by(name: "user3", username: "handle3", email: "u3@gmail.com", password: "password")
 
+# Samples follows received by user1
 f1 = Follow.find_or_create_by(followee: u1, follower: u2, accepted: false)
-f2 = Follow.find_or_create_by(followee: u2, follower: u1, accepted: false)
-f3 = Follow.find_or_create_by(followee: u1, follower: u3, accepted: true)
+f2 = Follow.find_or_create_by(followee: u1, follower: u3, accepted: true)
+
+# Sample follows sent by user1
+f3 = Follow.find_or_create_by(followee: u2, follower: u1, accepted: false)
+f4 = Follow.find_or_create_by(followee: u3, follower: u1, accepted: true)
+
+puts "Done importing mock data"
