36d81e5c541aefc1c6e922ff5031237d9c4740c4
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-30 07:11:15 +1100

    Removed accepted field from follows

--- db/migrate/20231228105827_create_follows.rb
+++ db/migrate/20231228105827_create_follows.rb
@@ -3,7 +3,6 @@ class CreateFollows < ActiveRecord::Migration[7.1]
     create_table :follows do |t|
       t.references :follower, null: false, foreign_key: { to_table: :users }
       t.references :followee, null: false, foreign_key: { to_table: :users }
-      t.boolean :accepted, null: false, default: false
 
       t.timestamps
     end
