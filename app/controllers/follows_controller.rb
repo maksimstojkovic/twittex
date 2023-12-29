d1633cd7cb19a74ec99e621dd885579f950802ba
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-30 10:48:53 +1100

    Rework follows controller params

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -5,10 +5,9 @@ class FollowsController < ApplicationController
     if Follow.exists?(follow_params)
         render_frame status: :conflict
     else
-      @follow = Follow.new(follow_params)
+      @follow = Follow.new(followee_id: follow_params[:followee], follower: current_user)
       
       if @follow.save
-        @followee = follow_params[:followee]
         render_frame
       else
         render_frame status: :unprocessable_entity
@@ -25,11 +24,12 @@ class FollowsController < ApplicationController
   private
     # Only allow a list of trusted parameters through.
     def follow_params
-      followee_id = params.require(:follow).permit(:user)[:user]
-      { followee: User.find(followee_id), follower: current_user }
+      params.require(:follow).permit(:followee)
     end
 
     def render_frame(**args)
-      render(partial: "follow_button", locals: { user: follow_params[:followee] }, **args)
+      render partial: "follow_button",
+        locals: { user: User.find(follow_params[:followee]) },
+        **args
     end
 end
