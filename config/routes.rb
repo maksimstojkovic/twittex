92af27846ec2d97756379630ff8ca2a22b95f9c4
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-29 16:04:48 +1100

    Add post scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_scope :user do
     root "devise/sessions#new"
   end
-
+  
   devise_for :users, controllers: { registrations: 'users/registrations' }
-
+  
+  resources :posts
+  
   #####
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
