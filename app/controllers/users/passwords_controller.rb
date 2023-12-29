aeefe41932a5fe702699810210e1e157136a587e
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-30 01:16:21 +1100

    Update password reset create action to not flag missing email addresses

---
+++ app/controllers/users/passwords_controller.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class Users::PasswordsController < Devise::PasswordsController
+  # GET /resource/password/new
+  # def new
+  #   super
+  # end
+
+  # POST /resource/password
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    yield resource if block_given?
+
+    if successfully_sent?(resource)
+      respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
+    else
+      if (resource.errors.full_messages - ["Email not found"]).empty?
+        flash[:notice] = t("devise.passwords.send_instructions")
+        respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
+      else
+        respond_with(resource)
+      end
+    end
+  end
+
+  # GET /resource/password/edit?reset_password_token=abcdef
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource/password
+  # def update
+  #   super
+  # end
+
+  # protected
+
+  # def after_resetting_password_path_for(resource)
+  #   super(resource)
+  # end
+
+  # The path used after sending reset password instructions
+  # def after_sending_reset_password_instructions_path_for(resource_name)
+  #   super(resource_name)
+  # end
+end
