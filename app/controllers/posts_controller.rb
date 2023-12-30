6f3e47094f5dcb6d0284f79926eca2a22ffb5d29
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-30 19:36:08 +1100

    Add turbo frame for post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
 
     if @post.save
-      redirect_to @post, notice: "Post was successfully created."
+      turbo_stream
     else
       render :new, status: :unprocessable_entity
     end
