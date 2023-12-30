6dcf35ae5ea37d2ce77374def6e2eb8e7de7f1bc
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-30 22:15:59 +1100

    Fix user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,58 +2,30 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   setup do
-    @user = users(:user1)
+    @user = users(:one)
   end
 
   test "should retrieve follows received" do
-    assert_equal @user.follows_received.length, 2
-    assert_includes @user.follows_received, follows(:pending_follower_request)
-    assert_includes @user.follows_received, follows(:accepted_follower_request)
-  end
-
-  test "should retrieve pending follower request" do
-    assert_equal @user.pending_follower_requests.length, 1
-    assert_includes @user.pending_follower_requests, follows(:pending_follower_request)
-  end
-
-  test "should retrieve accepted follower request" do
-    assert_equal @user.accepted_follower_requests.length, 1
-    assert_includes @user.accepted_follower_requests, follows(:accepted_follower_request)
-  end
-
-  test "should retrieve pending followers" do
-    assert_equal @user.pending_followers.length, 1
-    assert_includes @user.pending_followers, users(:user2)
+    assert_equal 2, @user.follows_received.length
+    assert_includes @user.follows_received, follows(:follower_one)
+    assert_includes @user.follows_received, follows(:follower_two)
   end
 
   test "should retrieve followers" do
-    assert_equal @user.followers.length, 1
-    assert_includes @user.followers, users(:user3)
+    assert_equal 2, @user.followers.length
+    assert_includes @user.followers, users(:two)
+    assert_includes @user.followers, users(:three)
   end
 
   test "should retrieve follows sent" do
     assert_equal @user.follows_sent.length, 2
-    assert_includes @user.follows_sent, follows(:pending_following_request)
-    assert_includes @user.follows_sent, follows(:accepted_following_request)
-  end
-
-  test "should retrieve pending following requests" do
-    assert_equal @user.pending_following_requests.length, 1
-    assert_includes @user.pending_following_requests, follows(:pending_following_request)
-  end
-
-  test "should retrieve accepted following requests" do
-    assert_equal @user.accepted_following_requests.length, 1
-    assert_includes @user.accepted_following_requests, follows(:accepted_following_request)
-  end
-
-  test "should retrieve pending followings" do
-    assert_equal @user.pending_followings.length, 1
-    assert_includes @user.pending_followings, users(:user2)
+    assert_includes @user.follows_sent, follows(:following_one)
+    assert_includes @user.follows_sent, follows(:following_two)
   end
 
   test "should retrieve followings" do
-    assert_equal @user.followings.length, 1
-    assert_includes @user.followings, users(:user3)
+    assert_equal @user.followings.length, 2
+    assert_includes @user.followings, users(:two)
+    assert_includes @user.followings, users(:three)
   end
 end
